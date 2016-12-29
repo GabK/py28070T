5f3fbc70399c73495b847a27c59b50ed5b92d43d
Gabriel Kenderessy <g@brie.lk>
2016-12-29 21:03:41 +0000

    Added ON/OFF commands for 3 outlets

---
+++ rf/__init__.py
@@ -0,0 +1,56 @@
+import time
+import RPi.GPIO as GPIO
+
+class Transmitter:
+    _CODE = '%s11011101111%s111111010111'
+
+    _LONG_HIGH = 0.0014583
+    _LONG_LOW = 0.00114583
+    _SHORT_HIGH = 0.0005859375
+    _SHORT_LOW = 0.000260416
+    _WAIT = 0.0139114583
+    _RETRIES = 10
+
+    _STATES = {
+        True : '01',
+        False : '10'
+    }
+
+    _SOCKETS = ['01', '10', '00']
+
+
+    data_pin = None
+    states = [None, None, None]
+
+    def __init__(self, data_pin, initial_state=[False, False, False]):
+        self.data_pin = data_pin
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.data_pin, GPIO.OUT)
+
+        if type(initial_state) is list:
+            for socket, state in enumerate(initial_state):
+                self.transmit(socket, state)
+
+    def set(self, socket, state):
+        code = self._CODE % self._SOCKETS[socket], self._STATES[state]
+
+        for attempt in range(self._RETRIES):
+            for b in code:
+                if b == '1':
+                    GPIO.output(self.data_pin, 1)
+                    time.sleep(self._SHORT_HIGH)
+                    GPIO.output(self.data_pin, 0)
+                    time.sleep(self._LONG_LOW)
+                elif b == '0':
+                    GPIO.output(self.data_pin, 1)
+                    time.sleep(self._LONG_HIGH)
+                    GPIO.output(self.data_pin, 0)
+                    time.sleep(self._SHORT_LOW)
+                else:
+                    continue
+
+            GPIO.output(self.data_pin, 0)
+            time.sleep(self._WAIT)
+
+        GPIO.cleanup()
---
+++ setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='Py28070T',
+
+    version='0.0.1',
+
+    description='Python driver for Nexxtech RF power outlets (using 28070T RF transmitter)',
+    long_description=long_description,
+
+    url='https://github.com/GabK/py28070T',
+
+    author='Gabriel Kenderessy',
+    author_email='g@brie.lk',
+
+    license='Apache-2.0',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: System :: Hardware :: Hardware Drivers',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='28070T nexxtech rf power outlet',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['RPi.GPIO'],
+    entry_points={
+        'console_scripts': [
+            'py28070T=py28070T:main',
+        ],
+    },
+)
